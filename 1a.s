324bd68fb0d8d037e37acf4671751de721f6597e
Justin Hu <justin.hu@alumni.ubc.ca>
2021-12-01 00:24:25 -0800

    fix cat
    need to use brk(0) to get actual start of heap under ASLR

--- 1a.s
+++ 1a.s
@@ -1,5 +1,3 @@
-extern _end
-
 section .text
 
 global _start:function
@@ -7,36 +5,41 @@ _start:
   ; get first argument
   mov rdi, [rsp + 16]
 
-  ; sub rsp, 0
-  ; locals:
-  ; none
-
   mov rax, 2 ; open inputFile
   ; mov rdi, rdi ; already have input file name
-  mov rsi, 0
+  mov rsi, rdonly
   mov rdx, 0
   syscall
 
   mov rdi, rax ; fstat opened inputFile
   mov rax, 5
-  mov rsi, statbuf
+  mov rsi, statBuf
   syscall
-  mov rdx, [statbuf + sizeOffset]
+  mov rdx, [statBuf + sizeOffset]
   mov r12, rdi
 
+  mov rax, 12 ; save heap start in rbx
+  mov rdi, 0
+  syscall
+  mov rbx, rax
+
   mov rax, 12 ; brk required space
-  lea rdi, [_end + rdx]
+  lea rdi, [rbx + rdx]
   syscall
 
   mov rax, 0 ; read file
   mov rdi, r12
-  mov rsi, _end
+  mov rsi, rbx
   ; mov rdx, rdx ; already have size in rdx
+  syscall
+
+  mov rax, 3 ; close file
+  mov rdi, r12
   syscall
 
   mov rax, 1 ; write file as a check
   mov rdi, 1
-  mov rsi, _end
+  mov rsi, rbx
   ; mov rdx, rdx ; already have size in rdx
   syscall
 
@@ -47,8 +50,12 @@ _start:
 section .rodata
 
 sizeOffset: equ 48
+rdonly: equ 0
 
 section .bss
 
-statbuf:
+statBuf:
   resb 144
+writeBuf:
+  resb 20
+writeBufEnd:
