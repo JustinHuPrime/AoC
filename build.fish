02b856945b43f755f937806ab5c71400650bdcdb
Justin Hu <justin.hu@alumni.ubc.ca>
2021-12-06 16:46:00 -0800

    add stop on fail to scripts

--- build.fish
+++ build.fish
@@ -1,5 +1,5 @@
 #!/usr/bin/env fish
 
 nasm -f elf64 $argv[1].s
-nasm -f elf64 ../common/common.s
-ld -o $argv[1] $argv[1].o ../common/common.o
+and nasm -f elf64 ../common/common.s
+and ld -o $argv[1] $argv[1].o ../common/common.o
--- run.fish
+++ run.fish
@@ -2,5 +2,5 @@
 
 set year (string split / (pwd))[-1]
 ../build.fish $argv[1]
-../get-input.fish $year (string sub -l 1 $argv[1])
-./$argv[1] (string sub -l 1 $argv[1]).txt
+and ../get-input.fish $year (string sub -l 1 $argv[1])
+and ./$argv[1] (string sub -l 1 $argv[1]).txt
