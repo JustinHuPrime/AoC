b67af2f8020e8ef3c027d14d990d136fe6023d21
Justin Hu <justin.hu@alumni.ubc.ca>
2021-12-09 21:30:53 -0800

    upgrade runner script to handle two-digit day numbers

--- run.fish
+++ run.fish
@@ -1,6 +1,7 @@
 #!/usr/bin/env fish
 
 set year (string split / (pwd))[-1]
+set day (string sub -l (math (string length $argv[1]) - 1) $argv[1])
 ../build.fish $argv[1]
-and ../get-input.fish $year (string sub -l 1 $argv[1])
-and ./$argv[1] (string sub -l 1 $argv[1]).txt
+and ../get-input.fish $year $day
+and ./$argv[1] $day.txt
