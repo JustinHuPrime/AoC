9f97187cf3229b0c5e5eded94d7acfa198c3d7d5
Justin Hu <justin.hu@alumni.ubc.ca>
2023-12-01 22:49:08 -0800

    Rewrote day 2 in haskell for fun

---
+++ 2023/2b-haskell.hs
@@ -0,0 +1,54 @@
+import Data.Text as Text (Text, drop, pack, split, strip, unpack)
+import System.Environment (getArgs)
+
+data Cubes = Cubes {red :: Integer, green :: Integer, blue :: Integer} deriving (Show)
+
+zeroCubes :: Cubes
+zeroCubes = Cubes 0 0 0
+
+redCubes :: Integer -> Cubes
+redCubes red = Cubes red 0 0
+
+greenCubes :: Integer -> Cubes
+greenCubes green = Cubes 0 green 0
+
+blueCubes :: Integer -> Cubes
+blueCubes blue = Cubes 0 0 blue
+
+maxCubes :: Cubes -> Cubes -> Cubes
+(Cubes r1 g1 b1) `maxCubes` (Cubes r2 g2 b2) =
+  Cubes (max r1 r2) (max g1 g2) (max b1 b2)
+
+data Game = Game {id :: Integer, cubes :: Cubes} deriving (Show)
+
+parseCube :: Text -> Cubes
+parseCube cube =
+  let [number, colour] = split (== ' ') cube
+   in case unpack colour of
+        "red" -> redCubes
+        "green" -> greenCubes
+        "blue" -> blueCubes
+        $ read
+        $ unpack number
+
+parseSet :: Text -> Cubes
+parseSet set =
+  foldr (maxCubes . parseCube) zeroCubes $
+    map strip $
+      split (== ',') set
+
+parseLine :: Text -> Game
+parseLine line =
+  let [idPrefix, sets] = split (== ':') line
+      [_, idNumber] = split (== ' ') idPrefix
+   in Game (read $ unpack idNumber) $
+        foldr (maxCubes . parseSet . strip) zeroCubes $
+          split (== ';') sets
+
+gamePower :: Game -> Integer
+gamePower (Game _ (Cubes r g b)) = r * g * b
+
+main :: IO ()
+main = do
+  file <- getArgs >>= readFile . head
+  print $ sum $ map (gamePower . parseLine . pack) $ lines file
